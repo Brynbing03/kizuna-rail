9ce708a3bb1a41fb5723c0ad3b168eb7fe345c91
Brynlee Bingham <[Brynbing03]@users.noreply.github.com>
2026-02-07 16:38:59 -0700

    task 3 complete! boo yeahgit add -A! i added query based filtering for the routs by region and seasson

--- src/routes/routes/list.js
+++ src/routes/routes/list.js
@@ -1,14 +1,29 @@
 import { getAllRoutes, getListOfRegions, getListOfSeasons } from '../../models/model.js';
 
 export default async (req, res) => {
-    const regions = await getListOfRegions();
-    const routes = await getAllRoutes();
-    const seasons = await getListOfSeasons();
+  const regions = await getListOfRegions();
+  const seasons = await getListOfSeasons();
 
-    res.render('routes/list', { 
-        title: 'Scenic Train Routes',
-        regions,
-        routes,
-        seasons
-    });
+  const { region = 'all', season = 'all' } = req.query;
+
+  let routes = await getAllRoutes();
+
+  //this filters by region if the user selects it 
+  if (region !== 'all') {
+    routes = routes.filter((r) => r.region === region);
+  }
+
+  //this does the same by filters by season
+  if (season !== 'all') {
+    routes = routes.filter((r) => r.bestSeason === season);
+  }
+
+  res.render('routes/list', {
+    title: 'Scenic Train Routes',
+    regions,
+    seasons,
+    routes,
+    // i did this so the select logic i did above works
+    query: { region, season },
+  });
 };
